3150db353f32f035859219ea4443b34409f48e81
Piotrek Rybiec <piotrrybiec28@gmail.com>
2024-10-05 22:30:06 +0200

    simple word operations

---
+++ jaxgpt/tokenizer.py
@@ -0,0 +1,36 @@
+import os
+import json
+import regex as re
+import requests
+
+import jax
+
+def bytes_to_unicode() -> dict[int, str]:
+    bs = list(range(ord("!"), ord("~")+1))+list(range(ord("¡"), ord("¬")+1))+list(range(ord("®"), ord("ÿ")+1))
+    cs = bs[:]
+    n = 0
+    for b in range(2**8):
+        if b not in bs:
+            bs.append(b)
+            cs.append(2**8+n)
+            n += 1
+    cs = [chr(n) for n in cs]
+    return dict(zip(bs, cs))
+
+def get_paris(word: str) -> set[tuple[str, str]]:
+    """
+    Return all bigrams as a set of tuples, of consecutive elements in the iterable word.
+    """
+    pairs = set()
+    prev_char = word[0]
+    for char in word[1:]:
+        pair = (prev_char, char)
+        print(pair)
+        pairs.add((prev_char, char))
+        prev_char = char
+    return pairs
+
+if __name__ == "__main__":
+    print(bytes_to_unicode())
+    word = "kocham Pati"
+    print(get_paris(word))
